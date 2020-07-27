533e1ca100d387c414c29dcca0f5c0d4da9f0f9d
paramono <alex@paramono.com>
2020-07-27 23:22:54 +0300

    Fix __init__.py

--- djpaypal_subs/__init__.py
+++ djpaypal_subs/__init__.py
@@ -1,2 +1,5 @@
-__version__ = '0.1.0'
+import pkg_resources
+from . import checks  # noqa: Register the checks
+
+__version__ = pkg_resources.require("djpaypal_subs")[0].version
 default_app_config = 'djpaypal_subs.apps.DjPaypalSubsConfig'
