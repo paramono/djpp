9207332dcd5c46bb91ff755bf26948e1d7549da1
paramono <alex@paramono.com>
2020-07-27 23:26:45 +0300

    Convert setup.py

--- setup.py
+++ setup.py
@@ -14,13 +14,15 @@ readme = ''
 setup(
     long_description=readme,
     name='djpaypal_subs',
-    version='0.1.0',
+    version='0.1.4',
     python_requires='==3.*,>=3.5.0',
     author='paramono',
     author_email='alex@paramono.com',
-    packages=['djpaypal_subs'],
+    packages=[
+        'djpaypal_subs', 'djpaypal_subs.migrations', 'djpaypal_subs.models'
+    ],
     package_dir={"": "."},
     package_data={},
-    install_requires=['attrs==19.*,>=19.3.0'],
+    install_requires=[],
     extras_require={"dev": ["pytest==5.*,>=5.2.0"]},
 )
