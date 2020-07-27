a84bb16117aa3e794495d1712a20fbbd49021e33
paramono <alex@paramono.com>
2020-07-27 16:54:31 +0300

    Move API handling to a separate module, change utils module

--- djpaypal_subs/utils.py
+++ djpaypal_subs/utils.py
@@ -1,107 +1,79 @@
-from django.conf import settings
+from decimal import Decimal
 
-import requests
-
-from .constants import (
-    APIMODE_SANDBOX, APIMODE_LIVE,
-    PAYPAL_API_BASE_URL_SANDBOX, PAYPAL_API_BASE_URL_LIVE, PAYPAL_API_BASE_URL,
-)
+from django.urls import NoReverseMatch, reverse
+from django.utils.html import format_html
 
 
-class classproperty(object):
-    def __init__(self, f):
-        self.f = f
-
-    def __get__(self, obj, owner):
-        return self.f(owner)
-
-
-class PaypalApi(object):
-    client_id = settings.PAYPAL_SUBSCRIPTIONS_CLIENT_ID
-    secret    = settings.PAYPAL_SUBSCRIPTIONS_SECRET
-    _token    = None
-
-    @classproperty
-    def token(cls):
-        if cls._token:
-            return cls._token
+def admin_urlify(column, help_text=None):  # pragma: no cover
+    """
+    Can be used to add a link to a model referenced in another admin.
+        Example:
+                fields = [admin_urlify("user")]
+    """
+    def inner(*args):
+        if len(args) > 1:
+            obj = args[1]
         else:
-            cls._token = cls.get_api_token()
-            return cls._token
+            obj = args[0]
+            _obj = getattr(obj, column)
+            if _obj is None:
+                return "-"
+            try:
+                url = _obj.get_absolute_url()
+            except (AttributeError, NoReverseMatch):
+                url = ""
+                admin_pattern = "admin:%s_%s_change" % (_obj._meta.app_label, _obj._meta.model_name)
+                admin_url = reverse(admin_pattern, args=[_obj.pk])
 
-    @classmethod
-    def build_endpoint(cls, endpoint, mode='settings'):
-        if mode == 'settings':
-            url = PAYPAL_API_BASE_URL + endpoint
-        elif mode == APIMODE_SANDBOX:
-            url = PAYPAL_API_BASE_URL_SANDBOX + endpoint
-        elif mode == APIMODE_LIVE:
-            url = PAYPAL_API_BASE_URL_LIVE + endpoint
-        return url
+                ret = format_html('<a href="{url}">{obj}</a>', url=admin_url, obj=_obj)
+                if url:
+                    ret += format_html(' (<a href="{url}">View</a>)', url=url)
+                return ret
 
-    @classmethod
-    def get_api_token(cls, client_id=None, secret=None, mode='settings', **kwargs):
-        endpoint = '/v1/oauth2/token'
-        url = cls.build_endpoint(endpoint, mode=mode)
-        headers = {
-            'Accept': 'application/json',
-            'Accept-Language': 'en_US',
-        }
-        data = dict(grant_type='client_credentials')
+        inner.short_description = column.replace("_", " ")
 
-        client_id = client_id or settings.PAYPAL_SUBSCRIPTIONS_CLIENT_ID
-        secret    = secret or settings.PAYPAL_SUBSCRIPTIONS_SECRET
-
-        # print(f'url: {url}')
-        # print(f'client_id: {client_id}')
-        # print(f'secret: {secret}')
-
-        r = requests.post(url, headers=headers, data=data, auth=(client_id, secret))
-        r.raise_for_status()
-        dic = r.json() or {}
-        return dic.get('access_token')
-
-    @classmethod
-    def list(cls, endpoint, mode='settings', **kwargs):
-        url = cls.build_endpoint(endpoint, mode=mode)
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer %s' % cls.token,
-        }
-        r = requests.get(url, headers=headers)
-        r.raise_for_status()
-        return r.json()
-
-    @classmethod
-    def list_products(cls, mode='settings', **kwargs):
-        endpoint = '/v1/catalogs/products'
-        return cls.list(endpoint, mode=mode, **kwargs)
-
-    @classmethod
-    def list_plans(cls, mode='settings', **kwargs):
-        endpoint = '/v1/billing/plans'
-        return cls.list(endpoint, mode=mode, **kwargs)
+        return inner
 
 
-def get_api_token(client_id=None, secret=None, mode='settings'):
-    endpoint = '/v1/oauth2/token'
-    if mode == 'settings':
-        url = PAYPAL_API_BASE_URL + endpoint
-    elif mode == APIMODE_SANDBOX:
-        url = PAYPAL_API_BASE_URL_SANDBOX + endpoint
-    elif mode == APIMODE_LIVE:
-        url = PAYPAL_API_BASE_URL_LIVE + endpoint
+def fix_django_headers(meta):
+    """
+    Fix this nonsensical API:
+        https://docs.djangoproject.com/en/1.11/ref/request-response/
+        https://code.djangoproject.com/ticket/20147
+    """
+    ret = {}
+    for k, v in meta.items():
+        if k.startswith("HTTP_"):
+            k = k[len("HTTP_"):]
+        elif k not in ("CONTENT_LENGTH", "CONTENT_TYPE"):
+            # Skip CGI garbage
+            continue
 
-    headers = {
-        'Accept': 'application/json',
-        'Accept-Language': 'en_US',
-    }
-    data = dict(grant_type='client_credentials')
+        ret[k.lower().replace("_", "-")] = v
 
-    client_id = client_id or settings.PAYPAL_SUBSCRIPTIONS_CLIENT_ID
-    secret    = secret or settings.PAYPAL_SUBSCRIPTIONS_SECRET
+        return ret
 
-    r = requests.post(url, headers=headers, data=data, auth=(client_id, secret))
-    r.raise_for_status()
-    dic = r.json() or {}
-    return dic.get('access_token')
+
+CURRENCY_SIGILS = {
+    "CAD": "$",
+    "EUR": "€",
+    "GBP": "£",
+    "USD": "$",
+}
+
+
+def get_friendly_currency_amount(amount, currency):
+    currency = currency.upper()
+    sigil = CURRENCY_SIGILS.get(currency, "")
+    # Always show two decimal places on the amount
+    # Note that amount is usually a string, so convert to Decimal first.
+    amount = "{:.2f}".format(Decimal(amount))
+    return "{sigil}{amount} {currency}".format(sigil=sigil, amount=amount, currency=currency)
+
+
+def get_version():
+    """
+    Returns the current dj-paypal version
+    """
+    from . import __version__
+    return __version__
