f8b6578577fa140712033155d15f6fc62eff710b
paramono <alex@paramono.com>
2020-07-28 12:57:46 +0300

    Update package version

--- setup.py
+++ setup.py
@@ -14,12 +14,14 @@ readme = ''
 setup(
     long_description=readme,
     name='djpaypal_subs',
-    version='0.1.5',
+    version='0.1.6',
     python_requires='==3.*,>=3.5.0',
     author='paramono',
     author_email='alex@paramono.com',
     packages=[
-        'djpaypal_subs', 'djpaypal_subs.migrations', 'djpaypal_subs.models'
+        'djpaypal_subs', 'djpaypal_subs.management',
+        'djpaypal_subs.management.commands', 'djpaypal_subs.migrations',
+        'djpaypal_subs.models'
     ],
     package_dir={"": "."},
     package_data={},
