a788b4682da05beae5e665c3c2a3c3b6bbf8f834
paramono <alex@paramono.com>
2020-07-28 22:09:04 +0300

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ readme = ''
 setup(
     long_description=readme,
     name='djpaypal_subs',
-    version='0.1.14',
+    version='0.1.15',
     python_requires='==3.*,>=3.5.0',
     author='paramono',
     author_email='alex@paramono.com',
